464169807959067f06bce267016fec65a678a4b7
KcirtapdiaZ <patrick.m.diaz@gmail.com>
2025-01-29 18:18:04 -0500

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,41 @@
+def list_divide(numbers, divide = 2):
+    count = 0
+    for number in numbers:
+        if number % divide == 0:
+            count += 1
+    return count
+# The function returns the number of elements in the numbers list that are divisibleby divide
 
-def list_divide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
-    pass
+class ListDivideException(Exception):
+        pass
+# The ListDivideException class is defined to handle exceptions that may occur when the list_divide function is called
 
 def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    # Test list_divide
+     
+# Test list_divide with the following test cases: Numbers divisible by 2
+    if list_divide([1,2,3,4,5]) != 2:
+        raise ListDivideException("Test case 1 failed!")
+
+# Test list_divide with the following test cases: Numbers divisible by 2
+    if list_divide([2,4,6,8,10]) != 5:
+        raise ListDivideException("Test case 2 failed!")
+
+# Test list_divide with the following test cases: Numbers divisible by 10
+    if list_divide([30, 54, 63,98, 100], divide=10) != 2:
+        raise ListDivideException("Test case 3 failed!")
+
+# Test list_divide with the following test cases: Empty list
+    if list_divide([]) != 0:
+        raise ListDivideException("Test case 4 failed!")
+
+# Test list_divide with the following test cases: Numbers divisible by 1
+    if list_divide([1,2,3,4,5], 1) != 5:
+        raise ListDivideException("Test case 5 failed!")    
+   
 if __name__ == "__main__":
-    testListDivide()
+    try:
+        test_list_divide()
+        print("All test cases passed!")
+    except ListDivideException as x:
+        print(f"An error occurred!: {x}")
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,22 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    def __init__(self, title, author):
+        self.title = title 
+#Set the title of the book
+        self.author = author 
+#Set the author of the book
+
     def display(self):
-        pass
+        print(self.author + ", written by " + self.title)
+#Print the author and title of the book in the format: author, written by title
 
 
 if __name__ == "__main__":
-    a = Book("Thorpe", "Beat the Dealer")
-    print("This is a assignment 1 part 2")
+    book1 = Book("J.K. Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+#Create two book objects with the title and author of the book
+
+    book1.display()
+    book2.display()
+#Display the author and title of the book
+
+    print("This is assignment 1 part")
